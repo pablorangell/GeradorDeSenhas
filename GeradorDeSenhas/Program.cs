6932ac20353987d23f7563ad18a67c53c4434866
Pablo Rangel <prangelldestroy@gmail.com>
2022-12-20 09:49:12 -0300

    Adicionar arquivos de projeto.

---
+++ GeradorDeSenhas/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+namespace GeradorDeSenhas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantidade = int.Parse(args[0]);
+            string caracteresPossiveis = 
+                "ABCDEFGHIJKLMNOPQRSTUVWXYZÇ" +
+                "ABCDEFGHIJKLMNOPQRSTUVWXYZÇ".ToLower() + 
+                "0123456789" + 
+                "%$#@!"
+                ;
+
+            Random sorteio = new Random();
+            int numeroSorteado = 0;
+            StringBuilder password= new StringBuilder();
+
+            for(int t = 0; t < quantidade; t++)
+            {
+                numeroSorteado = sorteio.Next(0, caracteresPossiveis.Length - 1);
+                password.Append(caracteresPossiveis[numeroSorteado]);
+            }
+            Console.WriteLine();
+            Console.Write(password);
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
+}
